4475f884518ce858bf5b60b655aa7f795c5c1f3d
Igor Gotlibovych <igor.gotlibovych@gmail.com>
2018-11-03 16:36:02 +0000

    close menu folders for small screens

--- static/js/menu.js
+++ static/js/menu.js
@@ -1,50 +1,60 @@
-(function() {
-    const classNameOpen = 'open';
+const classNameOpen = 'open';
+const screen_sm_max = 991;
 
-    function setOpen(element) {
-        var target = document.getElementById(element.getAttribute('data-open'));
-        var lists = target.getElementsByTagName('ul');
-        target.classList.add(classNameOpen);
-        for (var i=0; i<lists.length; i++) {
-            var ul = lists[i];
-            ul.style.display = 'block';
-        }
+// from https://github.com/ryanve/verge
+function viewportW() {
+    var win = typeof window != 'undefined' && window;
+    var doc = typeof document != 'undefined' && document;
+    var docElem = doc && doc.documentElement;
+    var a = docElem['clientWidth'], b = win['innerWidth'];
+    return a < b ? b : a;
+};
+      
+function setOpen(element) {
+    var target = document.getElementById(element.getAttribute('data-open'));
+    var lists = target.getElementsByTagName('ul');
+    target.classList.add(classNameOpen);
+    for (var i=0; i<lists.length; i++) {
+        var ul = lists[i];
+        ul.style.display = 'block';
     }
+}
 
-    function setClosed(element) {
-        var target = document.getElementById(element.getAttribute('data-open'));
-        var lists = target.getElementsByTagName('ul');
-        target.classList.remove(classNameOpen);
-        for (var i=0; i<lists.length; i++) {
-            var ul = lists[i]
-            ul.style.display = 'none';
-        }
+function setClosed(element) {
+    var target = document.getElementById(element.getAttribute('data-open'));
+    var lists = target.getElementsByTagName('ul');
+    target.classList.remove(classNameOpen);
+    for (var i=0; i<lists.length; i++) {
+        var ul = lists[i]
+        ul.style.display = 'none';
     }
+}
 
-    function init(element) {
-        var target = document.getElementById(element.getAttribute('data-open'));
-        if (target.classList.contains(classNameOpen)) {
-            setOpen(element);
-        } else {
-            setClosed(element);
-        }
+function init(element) {
+    var target = document.getElementById(element.getAttribute('data-open'));
+    if (target.classList.contains(classNameOpen) && (viewportW() > screen_sm_max)) {
+        setOpen(element);
+    } else {
+        setClosed(element);
     }
+}
 
-    function toggle(event) {
-        var element = event.currentTarget;
-        var target = document.getElementById(element.getAttribute('data-open'));
-        if (target.classList.contains(classNameOpen)) {
-            setClosed(element);
-        } else {
-            setOpen(element);
-        }
+function toggle(event) {
+    var element = event.currentTarget;
+    var target = document.getElementById(element.getAttribute('data-open'));
+    if (target.classList.contains(classNameOpen)) {
+        setClosed(element);
+    } else {
+        setOpen(element);
     }
+}
 
-    var clickableElements = document.querySelectorAll('[data-open]');
+var clickableElements = document.querySelectorAll('[data-open]');
 
-    for (var i=0; i<clickableElements.length; i++) {
-        var element = clickableElements[i];
-        init(element);
-        element.addEventListener('click', toggle);
-    }
-})();
+for (var i=0; i<clickableElements.length; i++) {
+    var element = clickableElements[i];
+    init(element);
+    element.addEventListener('click', toggle);
+}
+
+console.log('Initialized menu')
